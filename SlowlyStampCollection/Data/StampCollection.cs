2b318be5b51b3366e8c27a0b36d8c364ee44172e
t0xick0ala <toxicuskoala@gmail.com>
2020-12-07 11:07:49 +1100

    Init proj

---
+++ SlowlyStampCollection/Data/StampCollection.cs
@@ -0,0 +1,33 @@
+﻿
+namespace SlowlyStampCollection.Data
+{
+    public class Stamp
+    {
+        private static readonly string[] Colors = new[]
+        {
+            "gray", "black", "green", "blue", "purple", "red"
+        };
+
+        private static readonly string[] Difficulties = new[]
+        {
+            "Very Easy", "Easy", "Normal", "Hard", "Very Hard", "Impossible"
+        };
+
+        private static readonly string[] Categories = new[]
+        {
+            "Other", "Single Stamp", "Stamp Set", "Location Exclusive"
+        };
+        public string Slug { get; set; }
+        public string Url { get { return @"https://cdn.getslowly.com/assets/images/stamp-sm/" + Slug + @".png"; } }
+        public string StampName { get { return Slug + @".png"; } }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public string Method { get; set; }
+        public int Category { get; set; }
+        public string CategoryName { get { return Categories[Category]; } }
+        public int Difficulty { get; set; }
+        public string DifficultyCode { get { return Colors[Difficulty]; } }
+        public string DifficultyName { get { return Difficulties[Difficulty]; } }
+        public int Price { get; set; }
+    }
+}
---
+++ SlowlyStampCollection/Data/StampCollectionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SlowlyStampCollection.Data
+{
+    public class StampCollectionService
+    {
+        public Task<Stamp[]> GetStampAsync()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+
+            return Task.FromResult(JsonSerializer.Deserialize<Stamp[]>(File.ReadAllText(@"Data\stamps.json"), options).ToArray());
+        }
+    }
+}
